d41992386aadbfbc971ef92a5a85636957f8f436
ajay01chaturvedi <37304011+ajay01chaturvedi@users.noreply.github.com>
2019-02-20 05:07:06 +0530

    updated correct problem statement

--- 2018-may/1.adhoc thinking/src/com/alg/top20/duplicate/FindDuplicate.java
+++ 2018-may/1.adhoc thinking/src/com/alg/top20/duplicate/FindDuplicate.java
@@ -1,5 +1,5 @@
-//PROBLEM - Given an array of n elements which contains elements from 0 to n-1, with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space.
-//For example, let n be 7 and array be {1, 2, 3, 1, 3, 6, 6}, the answer should be 1, 3 and 6.
+//PROBLEM - Given an array of n elements which contains elements from 1 to n-1. Find an efficient algo to return duplicate interger.
+//For example, let n be 7 and array be {1, 2, 3, 3}, the answer should be 3.
 
 package com.alg.top20.duplicate;
 import java.util.Arrays;
